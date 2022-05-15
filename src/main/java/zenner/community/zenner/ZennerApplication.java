daf53ff4548ffa8f54621ab5489e4cbd27eac713
snow-seamless <72679894+snow-seamless@users.noreply.github.com>
2022-05-15 21:50:40 +0800

    init repo

---
+++ src/main/java/zenner/community/zenner/ZennerApplication.java
@@ -0,0 +1,13 @@
+package zenner.community.zenner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZennerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZennerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zenner/community/zenner/ZennerApplicationTests.java
@@ -0,0 +1,13 @@
+package zenner.community.zenner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZennerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
