c3711b0c4dec7c7d39e1d96cba7656c782b3a1c0
snowseamless <1363252243@qq.com>
2022-05-17 13:01:08 +0800

    âextract github config from code to properties

--- src/main/java/zenner/community/zenner/controller/AuthorizeController.java
+++ src/main/java/zenner/community/zenner/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package zenner.community.zenner.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,20 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id(("3068c6012134d819c205"));
-        accessTokenDto.setClient_secret("b77996c1b1c98487b56cd2d835a678c0b2f8288b");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
